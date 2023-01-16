11722156828a1d7f3f171dcddec709aba495dd09
JJ-VP <JJ-VP@users.noreply.github.com>
2023-01-16 23:29:13 +0000

    Rolling presence
    Set the presence and change it every 10 seconds

--- events/ready/consoleLog.js
+++ events/ready/consoleLog.js
@@ -1,3 +1,14 @@
-module.exports = (client) => {
-	console.log(`${client.user.tag} is online.`);
+const { ActivityType } = require("discord.js");
+
+module.exports = async (client) => {
+	const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+	console.log(`${client.user.tag} has logged in. \nReady to serve on ${client.guilds.cache.size} servers, for ${client.users.cache.size} users.`);
+	while (true) {
+		client.user.setPresence({ activities: [{ name: `over ${client.users.cache.size} users.`, type: ActivityType.Watching }], status: `dnd` });
+		await sleep(10000);
+		client.user.setPresence({ activities: [{ name: `over ${client.channels.cache.size} channels.`, type: ActivityType.Watching }], status: `dnd` });
+		await sleep(10000);
+		client.user.setPresence({ activities: [{ name: `over ${client.guilds.cache.size} servers.`, type: ActivityType.Watching }], status: `dnd` });
+		await sleep(10000);
+	}
 };
