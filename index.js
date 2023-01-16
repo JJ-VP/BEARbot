48fb6577039738a1a26ee2b2bbdfc0e45fe496a4
JJ-VP <JJ-VP@users.noreply.github.com>
2023-01-16 23:13:47 +0000

    Added exception handler
    Bad way of doing this but prevents app crashes

--- index.js
+++ index.js
@@ -8,15 +8,14 @@ const path = require("path");
 const eventHandler = require("./handlers/eventHandler");
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
-		GatewayIntentBits.GuildPresences,
-	],
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildPresences],
 });
 
 eventHandler(client);
+
+process.on("uncaughtException", (error) => {
+	client.users.cache.get(`176721924448059402`).send(`${error}`);
+	console.log(error);
+});
 
 client.login(process.env.TOKEN);
