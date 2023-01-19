ef271be8564aed44e15ac8efc4b43431a415fcdb
JJ-VP <JJ-VP@users.noreply.github.com>
2023-01-19 16:31:00 +0000

    Create errorHandler.js
    Created an errorHandler for better error catching / understanding

---
+++ handlers/errorHandler.js
@@ -0,0 +1,47 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+	error: async (client, error, guild, channel, user, interaction) => {
+		try {
+			let devs = ["176721924448059402"];
+			if (interaction) {
+				let size = Object.keys(interaction.options._hoistedOptions).length;
+				let options = "";
+				for (let i = 0; i < size; i++) {
+					options = options + `${interaction.options._hoistedOptions[i].name}: *${interaction.options._hoistedOptions[i].value}*\n`;
+				}
+				const embedReply = new EmbedBuilder()
+					.setColor("#ff0000")
+					.setTitle(`Error`)
+					.addFields({ name: "Command:", value: `${interaction.command.name}` }, { name: "Options:", value: `${options}` }, { name: "error:", value: `${error}` })
+					.setTimestamp()
+					.setFooter({ text: "Error logged", iconURL: `${client.user.avatarURL({ format: "png", dynamic: true, size: 512 })}` });
+
+				const embedDev = new EmbedBuilder()
+					.setColor("#ff0000")
+					.setTitle("Error")
+					.addFields({ name: "command", value: `${interaction.command.name}` }, { name: "Options:", value: `${options}` }, { name: "Guild:", value: `${guild}` }, { name: "Channel", value: `${channel}` }, { name: "User:", value: `${user}` }, { name: "Error:", value: `${error}` })
+					.setTimestamp()
+					.setFooter({ text: "Error logged", iconURL: `${client.user.avatarURL({ format: "png", dynamic: true, size: 512 })}` });
+
+				await interaction.reply({ embeds: [embedReply], ephemeral: true });
+				devs.forEach((dev) => {
+					client.users.cache.get(dev).send({ embeds: [embedDev] });
+				});
+			} else {
+				const embedDev = new EmbedBuilder()
+					.setColor("#ff0000")
+					.setTitle("Error")
+					.addFields({ name: "Error:", value: `${error}` })
+					.setTimestamp()
+					.setFooter({ text: "Error logged", iconURL: `${client.user.avatarURL({ format: "png", dynamic: true, size: 512 })}` });
+
+				devs.forEach((dev) => {
+					client.users.cache.get(dev).send({ embeds: [embedDev] });
+				});
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	},
+};
