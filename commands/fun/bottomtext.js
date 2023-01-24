3d2c403bcf3d5a4d41a91d683c37fa78b3f2ef77
JJ-VP <JJ-VP@users.noreply.github.com>
2023-01-24 01:33:47 +0000

    add bottomText command
    Added command to add text to the bottom(overlay) of an image

---
+++ commands/fun/bottomtext.js
@@ -0,0 +1,55 @@
+const { ApplicationCommandOptionType, AttachmentBuilder } = require("discord.js");
+const error = require("../../handlers/errorHandler.js");
+const Canvas = require("@napi-rs/canvas");
+
+module.exports = {
+	name: "bottomtext",
+	description: "Add text to the bottom of an image!",
+	options: [
+		{
+			name: "image",
+			description: "The image you want to add text too.",
+			required: true,
+			type: ApplicationCommandOptionType.Attachment,
+		},
+		{
+			name: "text",
+			description: "The text you want to add to the image.",
+			required: true,
+			type: ApplicationCommandOptionType.String,
+		},
+	],
+
+	callback: async (client, interaction) => {
+		try {
+			const file = interaction.options.getAttachment("image");
+			const text = interaction.options.getString("text");
+			const extension = file.name.split(".").pop().toLowerCase();
+			const allowedTypes = ["png", "jpg", "jpeg", "avif", "webp"];
+			if (allowedTypes.indexOf(extension) < 0) return interaction.reply({ content: `The filetype you surplied is not supported by this command (png, jpg, jpeg, avif, webp)`, ephemeral: true });
+
+			const canvas = Canvas.createCanvas(file.width, file.height);
+			const context = canvas.getContext("2d");
+			const background = await Canvas.loadImage(`${file.url}`);
+			context.drawImage(background, 0, 0, canvas.width, canvas.height);
+			const applyText = (canvas, text) => {
+				const context = canvas.getContext("2d");
+				let fontSize = 100;
+				do {
+					context.font = `${(fontSize -= 1)}vw`;
+				} while (context.measureText(text).width > canvas.width / 1.1);
+				return context.font;
+			};
+			context.font = applyText(canvas, text);
+			context.fillStyle = `#ffffff`;
+			context.textAlign = "center";
+			context.fillText(text, canvas.width / 2, canvas.height / 1.1);
+			const Attachment = new AttachmentBuilder(await canvas.encode(`png`), { name: `bottomText.png` });
+			interaction.reply({ files: [Attachment] });
+			//I don't like how this works... but it works.
+		} catch (e) {
+			console.log(e);
+			error.error(client, e, interaction);
+		}
+	},
+};
