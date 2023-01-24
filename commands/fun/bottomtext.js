63f4876a4cfb8cee29d7f5de4eea410ae0189c10
JJ-VP <JJ-VP@users.noreply.github.com>
2023-01-24 01:49:44 +0000

    Update bottomtext.js
    Formatted to generate bottom text memes

--- commands/fun/bottomtext.js
+++ commands/fun/bottomtext.js
@@ -25,25 +25,26 @@ module.exports = {
 			const file = interaction.options.getAttachment("image");
 			const text = interaction.options.getString("text");
 			const extension = file.name.split(".").pop().toLowerCase();
-			const allowedTypes = ["png", "jpg", "jpeg", "avif", "webp"];
-			if (allowedTypes.indexOf(extension) < 0) return interaction.reply({ content: `The filetype you surplied is not supported by this command (png, jpg, jpeg, avif, webp)`, ephemeral: true });
+			const allowedTypes = ["png", "jpg", "jpeg", "avif", "webp", "gif"];
+			if (allowedTypes.indexOf(extension) < 0) return interaction.reply({ content: `The filetype you surplied is not supported by this command :png, jpg, jpeg, avif, webp, gif(animated soon)`, ephemeral: true });
 
 			const canvas = Canvas.createCanvas(file.width, file.height);
 			const context = canvas.getContext("2d");
 			const background = await Canvas.loadImage(`${file.url}`);
 			context.drawImage(background, 0, 0, canvas.width, canvas.height);
 			const applyText = (canvas, text) => {
-				const context = canvas.getContext("2d");
 				let fontSize = 100;
 				do {
-					context.font = `${(fontSize -= 1)}vw`;
+					context.font = `${(fontSize -= 1)}px Impact`;
 				} while (context.measureText(text).width > canvas.width / 1.1);
 				return context.font;
 			};
 			context.font = applyText(canvas, text);
-			context.fillStyle = `#ffffff`;
+			context.fillStyle = "white";
 			context.textAlign = "center";
-			context.fillText(text, canvas.width / 2, canvas.height / 1.1);
+			context.fillText(text, canvas.width / 2, canvas.height / 1.05);
+			context.strokeStyle = "black";
+			context.strokeText(text, canvas.width / 2, canvas.height / 1.05);
 			const Attachment = new AttachmentBuilder(await canvas.encode(`png`), { name: `bottomText.png` });
 			interaction.reply({ files: [Attachment] });
 			//I don't like how this works... but it works.
