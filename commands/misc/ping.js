8b8ec13a7c75f5202c94a98331a1cdee7ba08066
JJ-VP <JJ-VP@users.noreply.github.com>
2023-02-02 17:05:06 +0000

    Ping v2
    Version 2 of the ping command, now featuring the client ping as well as the old websocket ping.

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -1,17 +1,34 @@
+const { ApplicationCommandOptionType } = require("discord.js");
 const error = require("../../handlers/errorHandler.js");
 
 module.exports = {
 	name: "ping",
 	description: "Pong!",
+	options: [
+		{
+			name: "ephemeral",
+			description: "Should the bot respond silently?",
+			type: ApplicationCommandOptionType.Boolean,
+		},
+	],
+	// name: String,
+	// description: String,
 	// devOnly: Boolean,
 	// testOnly: Boolean,
 	// options: Object[],
 	// deleted: Boolean,
 
 	callback: async (client, interaction) => {
-		await interaction.deferReply();
+		const ephemeral = interaction.options.getBoolean("ephemeral") || false;
+		if (ephemeral) {
+			await interaction.deferReply({ ephemeral: true });
+		} else {
+			await interaction.deferReply();
+		}
 		try {
-			interaction.editReply(`Pong! ${client.ws.ping}ms`);
+			const reply = await interaction.fetchReply();
+			const ping = reply.createdTimestamp - interaction.createdTimestamp;
+			interaction.editReply(`Client: ${ping}ms\nWebsocket: ${client.ws.ping}ms`);
 		} catch (e) {
 			error.error(client, e, interaction);
 		}
