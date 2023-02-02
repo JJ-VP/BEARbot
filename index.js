4caecd8b0f9aabfd073f28f2836cbb573b105f82
JJ-VP <JJ-VP@users.noreply.github.com>
2023-02-02 17:03:49 +0000

    indev command deletion
    Started ability to remove command for future use, no complete for commented out.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 
 const { Routes } = require("discord-api-types/v10");
-const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const { Client, GatewayIntentBits, REST } = require("discord.js");
+
+const rest = new REST({ version: 10 }).setToken(process.env.TOKEN);
 
 const fs = require("fs");
 const path = require("path");
@@ -19,3 +21,14 @@ process.on("uncaughtException", (error) => {
 });
 
 client.login(process.env.TOKEN);
+/*
+rest
+	.put(Routes.applicationCommands(`882711984733319210`), { body: [] })
+	.then(() => console.log(`Deleted all commands.`))
+	.catch(console.error);
+
+rest
+	.put(Routes.applicationGuildCommands(`882711984733319210`, `407627181305626654`), { body: [] })
+	.then(() => console.log(`Deleted all testServer commands.`))
+	.catch(console.error);
+*/
